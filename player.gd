e97650f19e6a055624029024a30fe8c132d726c3
Louis-Philippe L'Écuyer <lplecuyer@hotmail.com>
2024-10-26 11:48:45 -0400

    player input + animation

---
+++ player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+# Vitesse de déplacement du personnage
+var speed: float = 200.0
+@onready var _animation_player = $AnimationPlayer
+
+
+func _process(delta: float) -> void:
+	
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+
+
+	input_vector = input_vector.normalized()
+
+	
+	velocity = input_vector * speed
+	move_and_slide()
+	
+	if input_vector != Vector2.ZERO:
+		if not _animation_player.is_playing(): 
+			_animation_player.play("walk") 
+	else:  
+			_animation_player.play("RESET")
